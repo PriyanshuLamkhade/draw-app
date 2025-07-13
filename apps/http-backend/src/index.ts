4dba78d78912d4fe189ed5e38a5db673932db898
Priyanshu <priyanshulamkhade@gmail.com>
2025-07-13 10:31:06 +0530

    signup ep

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,27 +1,39 @@
 import express from "express";
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
-import {JWT_SECRET} from "@repo/backend-common/config"
-import {CreateUserSchema} from "@repo/common/types"
-
+import { JWT_SECRET } from "@repo/backend-common/config"
+import { CreateUserSchema, CreateRoomSchema, SigninSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/db";
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-         res.json({message:"Incorrect Inputs"})
-         return
+    const parseData = CreateUserSchema.safeParse(req.body);
+    if (!parseData.success) {
+        res.json({ message: "Incorrect Inputs" })
+        return
     }
-    //db call
-    res.json({
-        userId:123
-    })
+    try {
+        await prismaClient.user.create({
+            data: {
+                email: parseData.data.username,
+                password: parseData.data.password,
+                name: parseData.data.name
+            }
+        })
+
+        res.json({
+            userId: 123
+        })
+    } catch (error) {
+        res.status(411).json(error)
+    }
+
 })
 app.post("/signin", async function (req, res) {
     const { username, password } = req.body
 
-    const user = {_id : 1}; //db call
+    const user = { _id: 1 }; //db call
     const token = jwt.sign({
         id: user._id
     }, JWT_SECRET)
@@ -29,10 +41,10 @@ app.post("/signin", async function (req, res) {
         token
     })
 })
-app.post("/rooms",middleware, async function (req, res) {
+app.post("/rooms", middleware, async function (req, res) {
     //db call
     res.json({
-        roomId:123
+        roomId: 123
     })
 })
 
