c21b2912ca59cab827c2dfb335a4c53f2726b87a
Priyanshu <priyanshulamkhade@gmail.com>
2025-08-15 00:56:36 +0530

    fixed color pallet

--- apps/excaliraw-frontend/components/ColorPallet.tsx
+++ apps/excaliraw-frontend/components/ColorPallet.tsx
@@ -1,4 +1,4 @@
-import { Check } from "lucide-react";
+import { Check, Minus } from "lucide-react";
 import { useRef, useState } from "react";
 interface ColorPalletInterface  {
   strokeColor: string;
@@ -18,9 +18,9 @@ export function ColorPallet({
   
 
   return (
-    <div>
+    <div className="flex">
       <div
-        className={`bg-zinc-800 flex flex-col gap-4 px-4 py-2 rounded-b-lg w-77 ml-5 border border-t-0 border-zinc-700 ${
+        className={`bg-zinc-800 flex flex-col gap-3 px-4 py-2 rounded-lg border-l-0 ml-5 border border-t-0 border-zinc-700 ${
           selectedTool === "hand" || selectedTool === "mouse" || selectedTool === "img" ? "hidden" : ""
         }`}
       >
@@ -41,20 +41,17 @@ export function ColorPallet({
             }}
           ></div>
         ))}
-        <p className="text-gray-400">|</p>
-
-        <div
-          style={{ "--current-color": strokeColor } as React.CSSProperties}
+        <p className="text-gray-400"><Minus color="white" /></p>
+        
+        <div style={{ "--current-color": strokeColor } as React.CSSProperties}
           className="h-6 w-7 rounded border bg-[var(--current-color)] border-gray-600 hover:scale-110 cursor-pointer"
-          onClick={() => setBoxClicked((x: boolean) => !x)}
-        ></div>
+          onClick={() => setBoxClicked((x: boolean) => !x)}></div>
       </div>
-
+        
       <div
         className={`${
           !boxClicked ? "hidden" : ""
-        } flex bg-white max-w-56 border border-zinc-700 rounded-md gap-1 m-auto p-1 justify-evenly`}
-      >
+        } flex h-10 bg-white border border-zinc-700 rounded-md gap-1 p-1 justify-evenly`}>
         <input
           ref={colorRef}
           type="text"
